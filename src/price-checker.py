97d8d3fd6e23bb5d2c43c1863f584ed7486ec27d
AlexMcLean123 <39857102+AlexMcLean123@users.noreply.github.com>
2019-07-20 15:32:49 +0100

    Update price-checker.py

--- src/price-checker.py
+++ src/price-checker.py
@@ -2,7 +2,7 @@ import requests;
 from bs4 import BeautifulSoup
 import smtplib
 import time 
-URL = 'https://www.amazon.co.uk/Dell-SE2219H-21-5-LED-backlit-Monitor/dp/B07HS74H5P/ref=sr_1_1?crid=969YI153MGUK&keywords=dell+se2219h+21.5+inch+ips+led-backlit+lcd+2019+monitor&qid=1563629970&s=gateway&sprefix=dell+se22%2Caps%2C185&sr=8-1'
+URL = 'Enter Url'
 
 headers = {"User-Agent" : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'}
 
